74a42fbf8ba75eacfec4fff4fedd1a190cd019a4
rafael <rafael.alves.ribeiro@gmail.com>
2019-04-09 04:42:28 -0300

    fix

--- app/__init__.py
+++ app/__init__.py
@@ -12,6 +12,7 @@ def connect_db():
     DB_USER = os.environ["DB_USER"]
     DB_PASSWORD = os.environ["DB_PASSWORD"]
     DB_HOST = "cognitivo.cfz4dq8w2iii.us-east-1.rds.amazonaws.com"
+    print(DB_USER, DB_PASSWORD, DB_HOST)
     con = psycopg2.connect(
         user=DB_USER, password=DB_PASSWORD, host=DB_HOST, port=5432, dbname="cognitivo"
     )
@@ -20,7 +21,10 @@ def connect_db():
 
 @app.route("/")
 def top_apps():
-    #con = connect_db()
+    try:
+        con = connect_db()
+    except:
+        print('erro db')
     #cursor = con.cursor()
     #sql = "SELECT * FROM app.citacoes-2019-04-09"
     #cursor.execute(sql)
