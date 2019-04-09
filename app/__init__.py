09e54b2ee7edf582af5a3613fe282c6922b9e7dc
rafael <rafael.alves.ribeiro@gmail.com>
2019-04-09 04:28:27 -0300

    query sql

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,28 @@
 import os
-from flask import Flask
+
+from flask import Flask, jsonify
+import psycopg2
+
 
 # Initialize application
 app = Flask(__name__)
 
 
-@app.route('/')
+def connect_db():
+    DB_USER = os.environ["DB_USER"]
+    DB_PASSWORD = os.environ["DB_PASSWORD"]
+    DB_HOST = "cognitivo.cfz4dq8w2iii.us-east-1.rds.amazonaws.com"
+    con = psycopg2.connect(
+        user=DB_USER, password=DB_PASSWORD, host=DB_HOST, port=5432, dbname="cognitivo"
+    )
+    return con
+
+
+@app.route("/")
 def top_apps():
-    return 'top_apps'
+    con = connect_db()
+    cursor = con.cursor()
+    sql = "SELECT * FROM app.citacoes-2019-04-09"
+    cursor.execute(sql)
+    data = cursor.fetchall()
+    return jsonify(data)
